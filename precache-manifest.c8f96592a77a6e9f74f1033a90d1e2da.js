29e728154dba5cbd4210505b8d8dcc6e7dc1cfe0
mohammedtalyb <mohammed.talib@raweng.com>
2019-06-14 12:39:40 +0530

    Updates

---
+++ precache-manifest.c8f96592a77a6e9f74f1033a90d1e2da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf58b8d57a344057d935cf42afff9c53",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "22f2d656f7400465d610",
+    "url": "/burger-builder/static/css/main.79e2c488.chunk.css"
+  },
+  {
+    "revision": "aecd56cd4f395d5ad7f2",
+    "url": "/burger-builder/static/js/2.17fe79c6.chunk.js"
+  },
+  {
+    "revision": "22f2d656f7400465d610",
+    "url": "/burger-builder/static/js/main.5bcce4b6.chunk.js"
+  },
+  {
+    "revision": "6168d9880ae57e7f6b00",
+    "url": "/burger-builder/static/js/runtime~main.ad75294f.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/27.1 burger-logo.png.b8503d26.png"
+  }
+]);
